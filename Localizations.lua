525e76d3cea9d0791023f191cf45b28301a35798
Hizuro <hizuro@gmx.net>
2022-05-16 19:28:08 +0200

    minor changes in localization

--- Localizations.lua
+++ Localizations.lua
@@ -8,73 +8,27 @@ ns.L = setmetatable(L,{__index=function(t,k)
 end});
 
 -- Do you want to help localize this addon?
--- https://www.curseforge.com/wow/addons/gathermate2_importexport/localization
-
---@do-not-package@
-L["AddOnLoaded"] = "AddOn loaded..."
-L["ExportByExpansion"] = "Export by expansion"
-L["ExportByZone"] = "Export by zone"
-L["InsertString"] = "Insert the export string here with Ctrl+V"
-L["ExportString"] = "Export string"
-L["ExportStringCreate"] = "Create export string"
-L["ExportStringCopy"] = "Copy export string with Ctrl+C";
-L["InputDialog"] = "Open the input dialog"
-L["InputDialogDesc"] = "Click on the button and copy the export string into the input field."
-L["InsertSuccesfull"] = "Successfully added %d/%d nodes to GatherMate2."
-L["Import/Export"] = "Import/Export"
---@end-do-not-package@
+-- https://www.curseforge.com/wow/addons/@cf-project-name@/localization
 
 --@localization(locale="enUS", format="lua_additive_table", handle-subnamespaces="none", handle-unlocalized="ignore")@
-
 if LOCALE_deDE then
---@do-not-package@
-	L["AddOnLoaded"] = "AddOn geladen..."
-	L["ExportByExpansion"] = "Export nach Erweiterung"
-	L["ExportByZone"] = "Export nach Gebiet"
-	L["InsertString"] = "Füge die Export-Zeichenkette hier ein mit Strg+V"
-	L["ExportString"] = "Export-Zeichenkette"
-	L["ExportStringCreate"] = "Erzeuge Export-Zeichenkette"
-	L["ExportStringCopy"] = "Kopiere die Export-Zeichenkette mit Strg+C.";
-	L["InputDialog"] = "Öffne den Eingabe-Dialog"
-	L["InputDialogDesc"] = "Klick auf den Button und kopiere die Export-Zeichenkette in das Eingabefeld."
-	L["InsertSuccesfull"] = "Erfolgreich %d/%d Knoten GatherMate2 hinzugefügt."
-	L["Import/Export"] = "Import/Export"
---@end-do-not-package@
 --@localization(locale="deDE", format="lua_additive_table", handle-subnamespaces="none", handle-unlocalized="ignore")@
-end
-
-if LOCALE_esES then
+elseif LOCALE_esES then
 --@localization(locale="esES", format="lua_additive_table", handle-subnamespaces="none", handle-unlocalized="ignore")@
-end
-
-if LOCALE_esMX then
+elseif LOCALE_esMX then
 --@localization(locale="esMX", format="lua_additive_table", handle-subnamespaces="none", handle-unlocalized="ignore")@
-end
-
-if LOCALE_frFR then
+elseif LOCALE_frFR then
 --@localization(locale="frFR", format="lua_additive_table", handle-subnamespaces="none", handle-unlocalized="ignore")@
-end
-
-if LOCALE_itIT then
+elseif LOCALE_itIT then
 --@localization(locale="itIT", format="lua_additive_table", handle-subnamespaces="none", handle-unlocalized="ignore")@
-end
-
-if LOCALE_koKR then
+elseif LOCALE_koKR then
 --@localization(locale="koKR", format="lua_additive_table", handle-subnamespaces="none", handle-unlocalized="ignore")@
-end
-
-if LOCALE_ptBR or LOCALE_ptPT then
+elseif LOCALE_ptBR or LOCALE_ptPT then
 --@localization(locale="ptBR", format="lua_additive_table", handle-subnamespaces="none", handle-unlocalized="ignore")@
-end
-
-if LOCALE_ruRU then
+elseif LOCALE_ruRU then
 --@localization(locale="ruRU", format="lua_additive_table", handle-subnamespaces="none", handle-unlocalized="ignore")@
-end
-
-if LOCALE_zhCN then
+elseif LOCALE_zhCN then
 --@localization(locale="zhCN", format="lua_additive_table", handle-subnamespaces="none", handle-unlocalized="ignore")@
-end
-
-if LOCALE_zhTW then
+elseif LOCALE_zhTW then
 --@localization(locale="zhTW", format="lua_additive_table", handle-subnamespaces="none", handle-unlocalized="ignore")@
 end
